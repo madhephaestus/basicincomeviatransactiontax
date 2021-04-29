ff9e1ceb97fd1ceae3895072e96a745bd4753a32
Kevin Harrington <mad.hephaestus@gmail.com>
2021-04-29 10:47:55 -0400

    adding basic math

--- process.groovy
+++ process.groovy
@@ -1,1 +1,16 @@
 //Your code here
+
+int supplyChainDepth =46
+double annualIncome =24000
+double netMoneyCreation = annualIncome
+double transactionFeePercent=5.0
+double numberOfYears =5
+for (int i=0;i<supplyChainDepth;i++) {
+	
+	double transactionTax = (netMoneyCreation*transactionFeePercent/100.0)
+	netMoneyCreation=netMoneyCreation-transactionTax
+	double createdNetAnnual=netMoneyCreation/numberOfYears
+	double inflationPerceived =( createdNetAnnual/annualIncome)*100.0
+	println "Iteration "+i+" net anual created "+createdNetAnnual+" percentage inflation "+inflationPerceived+"%"
+	
+}
