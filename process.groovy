33e43ce885d12193219d0a03e2796c0e27419d47
Kevin Harrington <mad.hephaestus@gmail.com>
2021-04-29 10:53:12 -0400

    added comments

--- process.groovy
+++ process.groovy
@@ -1,10 +1,21 @@
-//Your code here
+/**
+ * Assume supply chain depth 
+ * Assume the number of years for the money to work its way through that supply chain
+ * Assume an annual wage
+ * Assume a transaction fee percentage
+ * This will check for the effect of the 
+ * net money creation, 
+ * annualized money creation, and 
+ * annualized inflation due to the money creation
+ * @author hephaestus
+ *
+ */
 
 int supplyChainDepth =46
 double annualIncome =24000
 double netMoneyCreation = annualIncome
-double transactionFeePercent=5.0
-double numberOfYears =5
+double transactionFeePercent=4.0
+double numberOfYears =14
 for (int i=0;i<supplyChainDepth;i++) {
 	
 	double transactionTax = (netMoneyCreation*transactionFeePercent/100.0)
